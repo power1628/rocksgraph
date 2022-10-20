ddeaf962a3c3afe807067b6d06a22058e33ccfde
pin.gao <pin.gao2008@gmail.com>
2022-10-20 14:52:59 +0800

    feat: kstep

--- src/graph/graph.rs
+++ src/graph/graph.rs
@@ -1,17 +1,36 @@
+use std::io::Write;
 use std::sync::Arc;
 use std::time::Instant;
 
 use anyhow::Result;
+use bytes::Buf;
+use crossbeam::channel::{unbounded, Receiver, Sender};
+use roaring::RoaringTreemap;
 use rocksdb::{self, BlockBasedOptions, DataBlockIndexType, IngestExternalFileOptions, Options};
+
+use super::r#type::{EdgeType, VertexId};
 
 pub const CF_VERTEX: &'static str = "vertex_cf";
 pub const CF_EDGE: &'static str = "edge_cf";
 type MTDB = rocksdb::DBWithThreadMode<rocksdb::MultiThreaded>;
 
+#[derive(Clone)]
 pub struct Graph {
     pub db_opts: Options,
     pub db_path: String,
     pub db: Arc<MTDB>,
+}
+
+type EdgeIter = Vec<VertexId>;
+
+fn raw_to_edge_iter(raw: Vec<u8>) -> EdgeIter {
+    let mut data = vec![];
+    let mut buf: &[u8] = raw.as_ref();
+    let size = data.len() / std::mem::size_of::<VertexId>();
+    for _ in 0..size {
+        data.push(buf.get_u64());
+    }
+    data
 }
 
 impl Graph {
@@ -78,5 +97,140 @@ impl Graph {
             .ingest_external_file_cf_opts(&cf, &ingest_opts, files)?;
         println!("ingest done cost {:?}", now.elapsed().as_secs_f32());
         Ok(())
+    }
+
+    pub fn seek_out_edge(&self, etype: EdgeType, from: VertexId) -> anyhow::Result<EdgeIter> {
+        let cf = self.db.cf_handle(CF_EDGE).unwrap();
+        let mut key = vec![];
+        key.write(from.to_be_bytes().as_ref());
+        let value = self.db.get_cf(&cf, key)?;
+        match value {
+            Some(raw) => Ok(raw_to_edge_iter(raw)),
+            None => Ok(EdgeIter::new()),
+        }
+    }
+
+    pub fn multi_get_out_edge(
+        &self,
+        etype: EdgeType,
+        froms: &Vec<VertexId>,
+    ) -> Vec<std::result::Result<Option<Vec<u8>>, rocksdb::Error>> {
+        let cf = self.db.cf_handle(CF_EDGE).unwrap();
+        let mut keys = vec![];
+        for from in froms {
+            let mut buf = vec![];
+            buf.write(from.to_be_bytes().as_ref());
+            keys.push((&cf, buf));
+        }
+
+        let values = self.db.multi_get_cf(keys);
+
+        values
+    }
+
+    pub fn decide_batch_size(data_size: usize) -> usize {
+        if data_size < 100 {
+            10
+        } else if data_size < 20480 {
+            128
+        } else {
+            512
+        }
+    }
+
+    pub async fn kstep(
+        &self,
+        etype: EdgeType,
+        from: VertexId,
+        step: usize,
+        num_tasks: usize,
+    ) -> std::result::Result<usize, rocksdb::Error> {
+        let mut frontier = vec![from];
+        for cur_step in 0..step {
+            let prev_front_size = frontier.len();
+            let _tt1 = std::time::Instant::now();
+            let mut prepare_cost = 0f64;
+
+            let now = std::time::Instant::now();
+
+            let (sx, rx) = unbounded();
+
+            let batch_size = Graph::decide_batch_size(frontier.len());
+
+            // spawn tasks
+            let mut tasks = Vec::new();
+            for _ in 0..num_tasks {
+                let rx: Receiver<Vec<VertexId>> = rx.clone();
+                let self_clone = self.clone();
+
+                let task = tokio::spawn(async move {
+                    let mut next_rtm = RoaringTreemap::new();
+                    let mut esize = 0usize;
+
+                    loop {
+                        match rx.recv() {
+                            Ok(srcs) => {
+                                let neis_set = self_clone.multi_get_out_edge(etype, &srcs);
+
+                                for neis in neis_set {
+                                    match neis {
+                                        Ok(value) => {
+                                            let raw = value.unwrap_or_default();
+                                            let mut buf: &[u8] = raw.as_ref();
+                                            let size = raw.len() / std::mem::size_of::<VertexId>();
+                                            for _ in 0..size {
+                                                next_rtm.insert(buf.get_u64());
+                                                esize += 1;
+                                            }
+                                        }
+                                        _ => {}
+                                    }
+                                }
+                            }
+                            _ => break,
+                        }
+                    }
+                    next_rtm
+                });
+                tasks.push(task);
+            }
+
+            // start sending
+            for batch in frontier.chunks(batch_size) {
+                let srcs = batch.to_vec();
+                let _ = sx.send(srcs).unwrap();
+            }
+            drop(sx);
+
+            // join results
+            let mut rtm = RoaringTreemap::new();
+            let mut join_cost = 0f64;
+            let mut convert_cost = 0f64;
+
+            for task in tasks {
+                let task_res = task.await.unwrap();
+                let tt = std::time::Instant::now();
+                rtm |= task_res;
+                join_cost += tt.elapsed().as_micros() as f64;
+            }
+            let tt = std::time::Instant::now();
+            // convert rtm to frontier
+            frontier.clear();
+            for datum in rtm.iter() {
+                frontier.push(datum);
+            }
+            convert_cost += tt.elapsed().as_micros() as f64;
+            println!("step {:?} batch_size {:?} frontier {:?} successor {:?} cost(micro) {:?} join {:?} convert {:?} prepare_cost {:?}",
+                                        cur_step,
+                                        batch_size,
+                                        prev_front_size,
+                                        frontier.len(),
+                                        now.elapsed().as_micros(),
+                                        join_cost,
+                                        convert_cost,
+                                        prepare_cost
+                                    );
+        }
+        Ok(frontier.len())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,15 @@ use std::sync::Arc;
 
 use clap::Parser;
 use rocksgraph::graph::graph::Graph;
+use rocksgraph::graph::r#type::EdgeType;
 use rocksgraph::graph::tools::importer::{GraphImporter, ImportOptions};
 use {anyhow, tokio};
+
 /// opts
 #[derive(Parser, Debug)]
 struct Cmd {
-    /// cmd: gensst, loadsst
-    #[clap(long, possible_values = &["gensst", "ingest"])]
+    /// cmd
+    #[clap(long, possible_values = &["gensst", "ingest", "kstep"])]
     cmd: String,
 
     /// rocksdb path
@@ -34,6 +36,18 @@ struct Cmd {
     /// delimiter
     #[clap(long, default_value = ",")]
     delimiter: char,
+
+    /// from vertex id
+    #[clap(long, default_value = "0")]
+    vid: u64,
+
+    /// kstep
+    #[clap(long, default_value = "1")]
+    kstep: usize,
+
+    /// query threads
+    #[clap(long, default_value = "10")]
+    thread: usize,
 }
 
 async fn gensst(graph: Arc<Graph>, cmd: &Cmd) {
@@ -53,6 +67,12 @@ async fn ingest(graph: Arc<Graph>, cmd: &Cmd) -> anyhow::Result<()> {
     graph.ingest_sst(cmd.output_dir.as_str())
 }
 
+async fn kstep(graph: Arc<Graph>, cmd: &Cmd) -> anyhow::Result<()> {
+    let etype = 1 as EdgeType;
+    let kstep_size = graph.kstep(etype, cmd.vid, cmd.kstep, cmd.thread).await?;
+    Ok(())
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let cmd: Cmd = Cmd::parse();
@@ -64,6 +84,8 @@ async fn main() -> anyhow::Result<()> {
         gensst(graph, &cmd).await;
     } else if cmd.cmd == "ingest" {
         ingest(graph, &cmd).await?;
+    } else if cmd.cmd == "kstep" {
+        kstep(graph, &cmd).await?;
     } else {
         println!("invalid cmd {:?}", cmd.cmd);
     }
